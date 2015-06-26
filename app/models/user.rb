11d347454f3a959a2a3208a48422dba7fc9149ce
Teri Pastorino <tpasto@comcast.net>
2015-06-26 06:59:22 -0700

    add validations email password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  attr_accessor :email, :password, :password_confirmation
+  validates_presence_of :email
+  validates_presence_of :password
   validates_uniqueness_of :email
 
 
