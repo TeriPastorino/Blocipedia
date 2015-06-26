16d9ea4f827cc1ccb4e295747adbe723c61f6501
Teri Pastorino <tpasto@comcast.net>
2015-06-26 06:56:16 -0700

    add user tests

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 describe "User registration" do 
+
   describe "without validation errors" do
     it "should have content 'Sign Up'" do
-      visit new_user_registration_path
+      visit signup_path
       expect(page).to have_content('Sign up')
     end
+
+    it "does not allow empty fields" do
+      visit signup_path
+      click_button 'Signup'
+      expect(page).to have_content("Password can't be blank")
+      expect(page).to have_content("Email can't be blank")
+    end
+
+
   end
   
 end
