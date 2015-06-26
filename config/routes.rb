afc6a140ef0015a601621cb60d8827eb04f7f8e8
Teri Pastorino <tpasto@comcast.net>
2015-06-26 06:55:00 -0700

    add shortcut routes login signout signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # root 'welcome#index'
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
