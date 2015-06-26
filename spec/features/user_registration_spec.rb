f0dea5256e17082aa170e962195c1ff79a06cf56
Teri Pastorino <tpasto@comcast.net>
2015-06-25 20:27:08 -0700

    install rspec

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "User registration" do 
   describe "without validation errors" do
     it "should have content 'Sign Up'" do
-      
+      visit new_user_registration_path
+      expect(page).to have_content('Sign up')
     end
   end
   
