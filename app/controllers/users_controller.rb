0a5d43901e38da456298858a7381eab44c8b15e4
Teri Pastorino <tpasto@comcast.net>
2015-06-28 21:11:41 -0700

    remember me and pw reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params[:user])
+    @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_url, flash: "You have successfully signed up"
+      UserMailer.registration_confirmation(@user).deliver flash[:notice] = "Please confirm your email address to continue"
+      redirect_to root_url
     else
+      flash[:error] = 'An error has occured'
       render :new
     end
+
+    # def confirm_email
+    #   user = User.find_by
+    # end
+
 
     def show
       @user = current_user
@@ -22,7 +28,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,31 @@
 class User < ActiveRecord::Base
+  before_create :confirmation_token
+  
   has_secure_password
-
-  attr_accessor :email, :password, :password_confirmation
   validates_presence_of :email
-  validates_presence_of :password
-  validates_uniqueness_of :email
+  validates :password, length: {minimum: 8}, allow_blank: true
+  before_create { generate_token(:auth_token) }
 
+  #send token for password reset
+  #add migration password_reset to users and reset_sent_at
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
 
+  #railscast
+  # def generate_token(column)
+  #   begin
+  #     self[column] = SecureRandom.urlsafe_base64
+  #   end while User.exists?(column: self[column])
+  # end
 
+  private
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
+  end
 end
