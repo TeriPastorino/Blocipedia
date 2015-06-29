021e4ed2b0847479cd94341d3f92d1546fd788e1
Teri Pastorino <tpasto@comcast.net>
2015-06-28 21:08:45 -0700

    add mailer for email and reset

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -2,20 +2,25 @@ require 'rails_helper'
 
 describe "User registration" do 
 
-  describe "without validation errors" do
-    it "should have content 'Sign Up'" do
-      visit signup_path
-      expect(page).to have_content('Sign up')
-    end
+  it "should have content 'Sign Up'" do
+    visit signup_path
+    expect(page).to have_content('Sign up')
+  end
 
+  describe "with validation errors" do
     it "does not allow empty fields" do
       visit signup_path
       click_button 'Signup'
       expect(page).to have_content("Password can't be blank")
       expect(page).to have_content("Email can't be blank")
     end
+  end
 
-
+  describe "without validation errors" do
+    # when I click the button, where do I redirect to?
+    it "redirects to (where?)"
+    # I expet that when i click the button it increases the User.count
+    it "saves the users"
   end
   
 end
