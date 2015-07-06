ab0149ef21d55f1d4142312a0a33064fe57cf7b4
Teri Pastorino <tpasto@comcast.net>
2015-07-06 10:08:50 -0700

    forms and formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver 
+      flash[:notice] = "Please confirm your email address to continue"
+      redirect_to root_url
+    else
+      flash[:error] = 'An error has occured'
+      render :new
+    end
+  end
+  
   def confirm_email
     user = User.find_by(confirm_token: params[:id])
     if user
       # set the user's confirm token to nil
-      user.email.activate      
+      user.email_activate      
       flash[:success] = "Welcome to Blocipedia your email has been confirmed. Sign in to continue"
       redirect_to root_url    
     else
-      flash[:error] = "Sorry. User does not exist."
+      flash[:error] = "Sorry, User does not exist!"
       redirect_to root_url
     end
   end
@@ -22,17 +34,6 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      UserMailer.registration_confirmation(@user).deliver 
-      flash[:notice] = "Please confirm your email address to continue"
-      redirect_to root_url
-    else
-      flash[:error] = 'An error has occured'
-      render :root_url
-    end
-  end
 
   private
   
