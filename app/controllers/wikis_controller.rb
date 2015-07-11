a271e512fa4d6ded584848e32bca6975dc3c2685
Teri Pastorino <tpasto@comcast.net>
2015-07-11 11:18:16 -0700

    style views, edit update

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,17 +1,18 @@
 class WikisController < ApplicationController
   def index
     @wikis = Wiki.all
+    # authorize @wikis
   end
 
   def new
     @wiki = Wiki.new
-    #authorize @wiki
+    authorize @wiki
   end
 
   def create
     @wiki = Wiki.new(wiki_params)
     @wiki.user = current_user
-    #authorize @wiki
+    authorize @wiki
     if @wiki.save
       flash[:notice] = "Your wiki was saved."
       redirect_to @wiki
@@ -23,31 +24,33 @@ class WikisController < ApplicationController
 
   def show
     @wiki = Wiki.find(params[:id])
-    #authorize @wiki
+    # authorize @wiki
   end
 
   def edit
     @wiki = Wiki.find(params[:id])
-    @users = User.all
-    authorize @wiki
+    #@users = User.all
+    # authorize @wiki
   end
 
   def update
     @wiki = Wiki.find(params[:id])
-    authorize @wiki
-    if wiki.update_attributes(wiki_params)
-      flash[:notice] = "Wiki has been updated"
+    @wiki.update_attributes(wiki_params)
+    # authorize @wiki
+    if @wiki.save
+      flash[:notice] = "Your changes have been made."
       redirect_to @wiki
     else
-      flash[:error] = "There was an error updating wiki, please try again."
-      render :edit
+      flash[:error] = "Error saving wiki. Please try again."
     end
   end
 
+
   def destroy
     @wiki = Wiki.find(params[:id])
-    if @wiki.destroy
-      flash[:notice] = "Wiki was deleted"
+    # authorize @wiki
+    if @wiki.delete
+      flash[:notice] = "\"#{@wiki.title}\" was deleted"
       redirect_to wikis_path
     else
       flash[:error] = "There was an error deleted the wiki. Please try again."
@@ -58,6 +61,6 @@ class WikisController < ApplicationController
   private 
 
   def wiki_params
-    params.require(:wiki).permit(:title, :body, :private)
+    params.require(:wiki).permit(:title, :body)
   end
 end
