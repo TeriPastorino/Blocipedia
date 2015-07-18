04861c497168b4a8d74a3acba6882941b1c446d9
Teri Pastorino <tpasto@comcast.net>
2015-07-18 14:38:27 -0700

    change application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -7,7 +7,7 @@ class ApplicationPolicy
   end
 
   def index?
-    true
+    false
   end
 
   def show?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    #user.present? && (record.user == user || user.admin)
+    false
   end
 
   def scope
