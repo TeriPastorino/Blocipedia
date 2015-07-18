f43616a390f3f4951cfe854c94af121cd3ee0717
Teri Pastorino <tpasto@comcast.net>
2015-07-18 14:12:42 -0700

    create markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,13 @@ module ApplicationHelper
       content_tag :div, capture(&block), class: 'form-group'
     end
   end
+
+  def markdown_wiki(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+  
 end
 
